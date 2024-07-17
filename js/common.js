63f99b1972323c583734c75cfb443603bd042b63
incheol <smu06030>
2024-07-17 12:42:54 +0900

    feat: 이미지 슬라이드 기능 개발

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// 상단 스크롤 바
 const progressBar = document.querySelector(".progressBar");
 const height =
   document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -7,4 +8,25 @@ window.addEventListener("scroll", () => {
   const scrolled = (scrollTop / height) * 100;
 
   progressBar.style.width = `${scrolled}%`;
+});
+
+// 이미지 슬라이드
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: "infinite",
+  autoplay: {
+    delay: 2000,
+  },
+
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
